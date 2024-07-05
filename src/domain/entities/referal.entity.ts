e07a9e882b9849244d2e6228e9730c8298281de6
Juan Andres Orozco <juanxxi2015@gmail.com>
2024-07-05 08:26:25 -0500

    Add the following entities

--- src/domain/entities/referal.entity.ts
+++ src/domain/entities/referal.entity.ts
@@ -3,7 +3,7 @@ import { Lead } from "./lead.entity";
 export class Referral {
     constructor(
       public id: string,
-      public lead: Lead, // Reference to the Lead who provided the referral
+      public lead: Lead,
       public name: string,
       public phone: string,
       public comments?: string,
